a903d54758a43800d5c363577b6f017231618fe4
kaito kuroba <3118659412@qq.com>
2022-10-25 15:04:47 +0800

    chore: add sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/css/main.scss";',
+        },
+      },
+    },
+  },
+  css:[
+    // CSS file in the project
+    '@/assets/css/main.css',
+  ]
 })
